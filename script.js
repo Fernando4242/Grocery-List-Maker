2de167d9b54bee9eb1538b4ed60afad37817625c
Vulpine <portillofernando92@gmail.com>
2022-01-03 22:50:38 -0600

    remvoe

--- script.js
+++ script.js
@@ -1,68 +1,128 @@
-//TODO: fix jsPDF format
-//TODO: fix array to match jsPDF format
-//TODO: clean up code and remove unnecessary code
-//FIXME: fix createFile function with jspdf regular table or auto-table
-const { jsPDF } = window.jspdf;
-import * as global from "./GlobalVariables.js";
-import enter_name from "./Button/enterName.js";
-import addItem from "./Button/addItem.js";
+/*
+  TO-DO:
+  - check max item count and generate new one
+  - make jspdf look prettier
+  - make functionality smoother
+  - make website prettier
+  - another interface 
+  - add old array into page as labels when user accepts to use old array
+  - embed pdf into webpage
+  - set starting id different
+  - add button to clear form
+  - add icons
+  - add footer
+  - redo remove function since it does not remove the correct one
+*/
 
-global.getFromLocalStorage();
+//using jsPDF to create a pdf file
+window.jsPDF = window.jspdf.jsPDF;
+
+//varibles used all around the code
+var nameInputText = document.getElementById("name-input");
+var topLeftName = document.getElementById("top-left-name");
+var centerName = document.getElementById("center-name");
+var enterNameButton = document.getElementById("enter-name-button");
+var createListButton = document.getElementById("create-list-button");
+var addItemButton = document.getElementById("add-item-button");
+var downloadButton = document.getElementById("download");
+var inputs = document.getElementById("inputs");
+var clearStartScene = document.getElementById("clear-start-scene");
+var listFoundScene = document.getElementById("list-found-scene");
+var creatingList = document.getElementById("working-on-list");
+var span = document.getElementById("range-text");
+var bar = document.getElementById("customRange2");
+var itemInput = document.getElementById("item-input");
+
+//get value list
+var div = document.getElementById("list");
+
+//grocery list to append input from user
+var grocerylist = [];
+var id = 0;
+var name = "";
+
+getFromLocalStorage();
+// localStorage.setItem("name",name);
 
 if (localStorage.getItem("name") != null) {
-  global.changeName(localStorage.getItem("name"));
+  name = localStorage.getItem("name");
 }
 
-if (global.grocerylist.length > 0) {
-  global.changeId(global.grocerylist.length);
-  global.listFoundScene.classList.remove("d-none");
+if (grocerylist.length > 0) {
+  id = grocerylist.length;
+  listFoundScene.classList.remove("d-none");
 
   var centerNameListFound = document.getElementById("center-name-list-found");
-  centerNameListFound.innerText = `Welcome back, ${global.name}!\n Would you like to create a new list or continue?`;
+  centerNameListFound.innerText = `Welcome back, ${name}!\n Would you like to create a new list or continue?`;
 } else {
-  global.clearStartScene.classList.remove("d-none");
+  clearStartScene.classList.remove("d-none");
 }
 
 function createListScene() {
-  global.listFoundScene.classList.add("d-none");
-  global.clearStartScene.classList.remove("d-none");
+  listFoundScene.classList.add("d-none");
+  clearStartScene.classList.remove("d-none");
   localStorage.clear();
 }
 
-global.saveToLocalStorage();
+saveToLocalStorage();
+
+//function enter_name() to get user input, to change text, and buttons
+function enter_name() {
+  //check if there is value : require a value
+
+  if (nameInputText.value.length != 0) {
+    //grab input
+    name = nameInputText.value;
+    grocerylist = [];
+    id = 0;
+
+    //change text and buttons
+    topLeftName.innerText = name + " Grocery List";
+
+    nameInputText.placeholder = "Ex: Milk (Character Limit : 40)";
+    clearStartScene.classList.add("d-none");
+    creatingList.classList.remove("d-none");
+    listFoundScene.classList.add("d-none");
+  } else {
+    //alert user
+    alert("Enter a value in the field");
+  }
+}
 
 function continueWithOldList() {
   //change text and buttons
-  global.topLeftName.innerText = global.name + "\n Grocery List";
+  topLeftName.innerText = name + " Grocery List";
 
   addItemsFromGroceryList();
 
-  global.clearStartScene.classList.add("d-none");
-  global.creatingList.classList.remove("d-none");
-  global.listFoundScene.classList.add("d-none");
+  nameInputText.placeholder = "Ex: Milk (Character Limit : 40)";
+  clearStartScene.classList.add("d-none");
+  creatingList.classList.remove("d-none");
+  listFoundScene.classList.add("d-none");
 }
 
-//FIXME: make work with newa array format
 function addItemsFromGroceryList() {
-  global.grocerylist.map((object) => {
-    var groceryItem = object.item;
-    var numItem = object.amount;
+  grocerylist.map((object) => {
+    var groceryItem = object[0];
+    var numItem = object[1];
 
     //create label to append
     var label = document.createElement("li");
-    label.id = object.id;
+    label.id = object[2];
 
     label.classList.add(
       "list-group-item",
       "d-flex",
       "justify-content-between",
-      "align-items-center",
-      "flex-wrap",
-      "p-2"
+      "align-items-center","flex-wrap","p-2"
     );
 
-    var number = document.createElement("p");
-    number.classList.add("badge", "bg-primary", "my-auto");
+    //create more tags to append into screen
+    // input = document.createElement("input"),
+    // span = document.createElement("p");
+    number = document.createElement("span");
+    number.classList.add("badge" ,"bg-primary", "my-auto");
+    // input.type = "checkbox";
 
     //change value according to user input on item
     label.innerHTML = groceryItem;
@@ -71,41 +131,36 @@ function addItemsFromGroceryList() {
     label.appendChild(number);
     label.onclick = function (e) {
       e.preventDefault();
-      var grocerylist = global.grocerylist;
 
-      var [x] = label.innerHTML.split("<", 1);
+      var [x] = (label.innerHTML).split('<', 1);
       console.log(x);
 
-      grocerylist = grocerylist.filter((obj) => {
-        return obj.item != x;
+      grocerylist = grocerylist.filter( value => {
+        return value[0] != x;
       });
 
-      global.changeGroceryList(grocerylist);
-      global.saveToLocalStorage();
-      global.calculateTotal();
+      saveToLocalStorage();
       label.remove();
     };
 
     //3. append label to the div
-    global.div.prepend(label);
+    div.prepend(label);
   });
-
-  global.calculateTotal();
 }
 
 //funciton create_list() start the process for the list
 function create_list() {
   //check if there is a value: require a value
 
-  if (global.nameInputText.value.length != 0) {
+  if (nameInputText.value.length != 0) {
     //change value
-    global.topLeftName.innerText = name + " Grocery List";
+    topLeftName.innerText = name + " Grocery List";
 
     //change button visibility
     changeButtonVisibility("2");
 
     //reset and change more text
-    global.nameInputText.value = "";
+    nameInputText.value = "";
     centerName.innerText = "Add Item To List";
     numItemText.classList.remove("hidden");
     downloadButton.classList.remove("hidden");
@@ -117,36 +172,203 @@ function create_list() {
   }
 }
 
+//function addItem() to add items (user input) to list and append to screen
+function addItem() {
+  //check if there is value : require a value
+  if (itemInput.value.length != 0 && !((itemInput.value).includes(";"))) {
+    //capatalize first letter
+    var groceryItem = capitalize(itemInput.value);
+    var numItem = parseInt(bar.value);
+
+    //create label to append
+    var label = document.createElement("li");
+    label.id = id;
+
+    label.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "align-items-center","flex-wrap" , "p-2"
+    );
+
+    //create more tags to append into screen
+    // input = document.createElement("input"),
+    // span = document.createElement("p");
+    number = document.createElement("p");
+    number.classList.add("badge" ,"bg-primary" , "my-auto");
+    // input.type = "checkbox";
+
+    let duplicateValue = checkDuplicate(groceryItem);
+
+    //check if value was entered
+    if (isNaN(numItem)) {
+      numItem = 1;
+    }
+
+    if (duplicateValue != -1) {
+      addToItem(duplicateValue, numItem);
+      changeLabelHTML(duplicateValue, grocerylist[duplicateValue][2]);
+      saveToLocalStorage();
+    } else {
+      //change value according to user input on item
+      label.innerHTML = groceryItem;
+      number.innerHTML = numItem;
+
+      //push to list
+      grocerylist.push([groceryItem, numItem, id]);
+      console.table(grocerylist);
+
+      //1. append input tag (checkbox) to label
+      // label.appendChild(input);
+
+      //2. append span tag (hold user input text) to label
+      // label.appendChild(span);
+
+      label.appendChild(number);
+      label.onclick = function (e) {
+        e.preventDefault();
+  
+        var [x] = (label.innerHTML).split('<', 1);
+        console.log(x);
+  
+        grocerylist = grocerylist.filter( value => {
+          return value[0] != x;
+        });
+  
+        saveToLocalStorage();
+        label.remove();
+      };
+
+      //3. append label to the div
+      div.prepend(label);
+
+      saveToLocalStorage();
+    }
+
+    id++;
+  } else {
+    //alert user
+    alert("Enter a valid input");
+  }
+
+  //reset and print to console for checking
+  itemInput.value = "";
+  bar.value = 1;
+  span.innerText = 1;
+}
+
+function removeItem({ e, label }) {
+  e.preventDefault();
+  console.log(label.innerHTML);
+
+  grocerylist = array.filter(function (value, index, arr) {
+    console.log(value);
+    return value[0] != label.innerHTML;
+  });
+
+  saveToLocalStorage();
+  label.remove();
+}
+
+//function create_file() to create a pdf file holding the values the user inputed (uses a library that was not written by me)
 function create_file() {
   //create variables needed to start
   var d = new Date();
   var doc = new jsPDF();
 
-  var headers = [{header: "Item", dataKey: "item"},{header: "Amount" , dataKey:'amount'}];
-
   //create text for date
-  var date = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+  date = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
 
-  // //set heading and size of title on the pdf file
+  //set heading and size of title on the pdf file
   doc.setFontSize(22);
   doc.text(20, 10, "Grocery List");
-  doc.text(150, 10, date);
-  
-  doc.autoTable({
-    headStyles: { fillColor: 0 },
-    margin: { top: 20 },
-    body: global.grocerylist,
-    columns: headers,
-  });
+  doc.text(90, 10, date);
+
+  var space = 20;
+
+  //loop over array and append to file
+  for (i = 0; i < grocerylist.length; i++) {
+    doc.text(20, (space += 10), grocerylist[i][0]);
+    doc.text(20, (space += 10), grocerylist[i][1].toString());
+  }
 
   // create, save and downloable to user
   var docName = date + "_grocerylist.pdf";
-  doc.save(docName);
+  // doc.save(docName);
+  doc.output("dataurlnewwindow");
 }
 
-//event listeners
-global.enterNameButton.addEventListener("click", enter_name);
-global.addItemButton.addEventListener("click", addItem);
-global.downloadButton.addEventListener("click", create_file);
-global.continueButton.addEventListener("click", continueWithOldList);
-global.clearListButton.addEventListener("click", createListScene);
+function changeButtonVisibility(option) {
+  switch (option) {
+    case "1":
+      // code block
+      nameInputText.classList.add("hidden");
+      enterNameButton.classList.add("hidden");
+      createListButton.classList.remove("hidden");
+      break;
+    case "2":
+      // code block
+      //change button visibility
+      createListButton.classList.add("hidden");
+      topLeftName.classList.remove("hidden");
+      nameInputText.classList.remove("hidden");
+      addItemButton.classList.remove("hidden");
+      break;
+  }
+}
+
+function checkDuplicate(item) {
+  var idFound = -1;
+
+  grocerylist.map((data, idx) => {
+    if (data[0] == item) {
+      idFound = idx;
+      return;
+    }
+  });
+
+  return idFound;
+}
+
+function capitalize(s) {
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+function addToItem(index, value) {
+  grocerylist[index][1] += value;
+}
+
+function changeLabelHTML(duplicateValue, index) {
+  var tempLabel = document.getElementById(`${index}`);
+  var tempChild = tempLabel.getElementsByTagName("p")[0];
+
+  //change innerHtml
+  tempChild.innerHTML = grocerylist[duplicateValue][1];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("List", JSON.stringify(grocerylist));
+  localStorage.setItem("name", name);
+  // //test save
+  // console.log(JSON.parse(localStorage.getItem("List")));
+}
+
+function getFromLocalStorage() {
+  console.log(localStorage.length);
+
+  if (localStorage.length == 2) {
+    var tempArray = JSON.parse(localStorage.getItem("List"));
+
+    if (tempArray != null) {
+      grocerylist = Array.from(tempArray);
+    }
+  }
+
+  console.log(grocerylist);
+}
+
+function createLabel() {}
+
+function handleChange() {
+  span.innerHTML = bar.value;
+}
